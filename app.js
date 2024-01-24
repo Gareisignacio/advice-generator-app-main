d40768ae5d831521319d5c194cbdbbc0c4d1ec45
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2024-01-24 12:37:42 -0300

    finished

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const dice = document.querySelector(".icon-dice");
 
+let card = document.querySelector(".card");
+let adviceNum = document.querySelector(".advice-num");
+let advicePara = document.querySelector(".advice-para");
+let patternDivider = document.querySelector(".pattern-divider");
 
-dice.addEventListener("click", ()=>{
-   alert("Dado")
-})
+dice.addEventListener("click", adviceChanger)
+
+function adviceChanger(){
+   fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(response=>{
+         adviceNum.textContent = `Advice #${response.slip.id}`
+         advicePara.textContent = `"${response.slip.advice}"`
+      })
+   
+   card.appendChild(adviceNum);
+   card.appendChild(advicePara);
+   card.appendChild(patternDivider)
+}
+
+adviceChanger();
